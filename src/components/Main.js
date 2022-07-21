84719dd80c04b4eb5a0ae51cfa8cbc7b32cd7091
özcan çetin <cetin445@gmail.com>
2022-07-21 22:13:42 +0300

    main control1

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,10 +8,13 @@ const Main = ({ item, getDetailMovies }) => {
   // const [movieId, setMovieId] = useState("")
   const IMG_API = "https://image.tmdb.org/t/p/w1280";
   return (
-    <div className="singleMovie" onClick={()=>getDetailMovies(item.id)}>
-      <Card style={{ width: "20rem" , height:"min-content"}} className="bg-info">
+    <div className="singleMovie" onClick={() => getDetailMovies(item.id)}>
+      <Card
+        style={{ width: "20rem", height: "min-content" }}
+        className="bg-info"
+      >
         <div className="movieImage">
-        <Card.Img variant="top" src={IMG_API + item.poster_path} />
+          <Card.Img variant="top" src={IMG_API + item.poster_path} />
           <div className="movieInfo">
             <h1>Overview</h1>
             <Card.Text>{item.overview}</Card.Text>
@@ -21,13 +24,8 @@ const Main = ({ item, getDetailMovies }) => {
           <div className="cardTitle">
             <Card.Title>{item.title}</Card.Title>
           </div>
-          
-
-          {/* <Button variant="primary">View More</Button> */}
         </Card.Body>
       </Card>
-
-      {/* <MovieDetail /> */}
     </div>
   );
 };

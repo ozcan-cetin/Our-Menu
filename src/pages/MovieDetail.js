8c1012870a054523348d0b32c26c6ecb35c2b0ca
özcan çetin <cetin445@gmail.com>
2022-07-25 17:06:28 +0300

    completed

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import VideoSection from '../components/VideoSection';
 
 const MovieDetail = ({item}) => {
   const {id} = useParams();
@@ -16,8 +17,8 @@ const MovieDetail = ({item}) => {
     vote_count,
   } = movieDetails;
 
-  const API_KEY = process.env.REACT_APP_TMDB_KEY;
-  // const API_KEY = "d6278b3dc3e6f8f8376a89851c3f8c8f";
+  // const API_KEY = process.env.REACT_APP_TMDB_KEY;
+  const API_KEY = "d6278b3dc3e6f8f8376a89851c3f8c8f";
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
   const baseImageUrl = 'https://image.tmdb.org/t/p/w1280';
@@ -38,7 +39,38 @@ const MovieDetail = ({item}) => {
   return (
     <div className='container py-5'>
       <h1 className='text-center'>{title}</h1>
-      {videoKey && }
+      {videoKey && <VideoSection videoKey={videoKey} />}
+      <div>
+      <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={poster_path ? baseImageUrl + poster_path : defaultImage}
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title">Overview</h5>
+              <p className="card-text">{overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                {'Release Date : ' + release_date}
+              </li>
+              <li className="list-group-item">{'Rate : ' + vote_average}</li>
+              <li className="list-group-item">
+                {'Total Vote : ' + vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1} className="card-link">
+                  Go Back
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

65659e989b301dac8443b45c68e510b4ded82e00
özcan çetin <cetin445@gmail.com>
2022-07-25 18:20:55 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,47 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { logOut } from "../auth/firebase";
-import { AuthContext } from "../context/AuthContext";
+import React from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { logOut } from '../auth/firebase';
+import { AuthContext } from '../context/AuthContext';
 
-const NavBar = () => {
+const Navbar = () => {
   const navigate = useNavigate();
-  const {currentUser} = useContext(AuthContext)
-  // const currentUser = {displayName:"özcan çetin"};
+  const { currentUser } = useContext(AuthContext);
+  //* with custom hook
+  // const { currentUser } = useAuthContext();
+  // const currentUser = { displayName: 'özcan çetin' };
   // const currentUser = false;
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-primary">
+      <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
-
-          <div className="d-flex text-white align-items-center">
+          <div className="d-flex text-white align-items-center ">
             {currentUser ? (
               <>
                 <h5 className="mb-0 text-capitalize">
                   {currentUser.displayName}
                 </h5>
-                <button className="btn bg-info ms-2" onClick={() => logOut()}>Logout</button>
+                <button
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => logOut()}
+                >
+                  Logout
+                </button>
               </>
             ) : (
               <>
                 <button
-                  className="btn bg-info ms-2"
-                  onClick={() => navigate("/login")}
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/login')}
                 >
                   Login
                 </button>
                 <button
-                  className="btn bg-info ms-2"
-                  onClick={() => navigate("/register")}
+                  className="ms-2 btn btn-outline-light"
+                  onClick={() => navigate('/register')}
                 >
                   Register
                 </button>
@@ -48,4 +54,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default Navbar;

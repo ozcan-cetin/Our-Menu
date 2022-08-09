27b5924f8d39bc4a843ff82f7ad24995685ab1b0
ozcan-cetin <98209250+ozcan-cetin@users.noreply.github.com>
2022-08-10 00:38:59 +0300

    Update Categories.js

--- src/Categories.js
+++ src/Categories.js
@@ -4,29 +4,23 @@ import Menu from './Menu';
 
 const Categories = ({menu, setMenu}) => {
 
-const [meal, setMeal] = useState(menu)
+const [data, setData] = useState(menu)
 
-console.log(meal);
 
-  const handleAll = () => {
-    setMenu(meal)
+const handleClick = (meal) => {
+  if(meal === "all"){
+    setMenu(data)
+  }else{
+    setMenu(data.filter((item)=>item.category===meal))
   }
-  const handleBreakfast = () => {
-    const breakfast = meal.filter((item)=>item.category==="breakfast")
-    setMenu(breakfast)
-  }
-  const handleLunch = () => {
-    setMenu(meal.filter((item)=>item.category==="lunch"))
-  }
-  const handleShakes = () => {
-    setMenu(meal.filter((item)=>item.category==="shakes"))
-  }
+}
+
 
   return <div className='d-flex justify-content-center mt-5 gap-1 mx-auto mx-3'>  
-    <button className='btn btn-outline-warning rounded-1' onClick={handleAll}>All</button>
-    <button className='btn btn-outline-warning rounded-1' onClick={handleBreakfast}>Breakfast</button>
-    <button className='btn btn-outline-warning rounded-1' onClick={handleLunch}>Lunch</button>
-    <button className='btn btn-outline-warning rounded-1' onClick={handleShakes}>Shakes</button>
+    <button className='btn btn-outline-warning rounded-1' onClick={(e)=>handleClick(e.target.innerText.toLowerCase())}>All</button>
+    <button className='btn btn-outline-warning rounded-1' onClick={(e)=>handleClick(e.target.innerText.toLowerCase())}>Breakfast</button>
+    <button className='btn btn-outline-warning rounded-1' onClick={(e)=>handleClick(e.target.innerText.toLowerCase())}>Lunch</button>
+    <button className='btn btn-outline-warning rounded-1' onClick={(e)=>handleClick(e.target.innerText.toLowerCase())}>Shakes</button>
   
   </div>;
 };
